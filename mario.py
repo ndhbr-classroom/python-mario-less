c83c3c6da8c4220197f5632b868954cbf3b17a02
Andreas Huber <info@ndhbr.de>
2021-12-03 23:20:38 +0100

    Updated exercise

--- mario.py
+++ mario.py
@@ -2,21 +2,7 @@
 def generate_mario_pyramid(height):
     pyramid = ''
 
-    # Check for individual numbers
-    if (height <= 0 or height > 8):
-        return
-
-    # Generate
-    for i in range(height):
-        # Loop to add spaces
-        for k in range(height - i, 1, -1):
-            pyramid += ' '
-        # Loop to add hashes
-        for j in range(0, i+1, 1):
-            pyramid += '#'
-        
-        if (i < height - 1):
-            pyramid += '\n'
+    # TODO: Build String Mario Pyramid
 
     # Print result
     return pyramid
