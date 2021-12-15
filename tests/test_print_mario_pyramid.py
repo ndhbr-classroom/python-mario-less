9aedb6025f1164c86aa0a979ff4700909ae9f72f
Andreas Huber <info@ndhbr.de>
2021-12-15 11:26:58 +0100

    Human readable error codes

--- tests/test_print_mario_pyramid.py
+++ tests/test_print_mario_pyramid.py
@@ -4,28 +4,37 @@ from mario import generate_mario_pyramid
 
 # Check if files exist
 def test_exists():
-    assert os.path.exists('mario.py')
-    assert os.path.exists('main.py')
+
+    err = "Die Datei mario.py existiert nicht"
+    assert os.path.exists('mario.py'), err
+
+    err = "Die Datei main.py existiert nicht"
+    assert os.path.exists('main.py'), err
 
 # Checks if accepts negative values
 def test_generate_negative():
-    assert generate_mario_pyramid(-1) == None
+    err = "Deine Funktion darf keine negative Werte akzeptieren"
+    assert generate_mario_pyramid(-1) == None, err
 
 # Checks if accepts 0
 def test_generate_zero():
-    assert generate_mario_pyramid(0) == None
+    err = "Deine Funktion darf den Wert 0 nicht akzeptieren"
+    assert generate_mario_pyramid(0) == None, err
 
 # Checks pyramid with height: 1
 def test_generate_one():
-    assert generate_mario_pyramid(1) == '#'
+    err = "Deine Funktion gibt für den Wert 1 eine falsche Pyramide aus"
+    assert generate_mario_pyramid(1) == '#', err
 
 # Checks pyramid with height: 2
 def test_generate_two():
+    err = "Deine Funktion gibt für den Wert 2 eine falsche Pyramide aus"
     assert generate_mario_pyramid(2) == ' #\n' \
-                                        '##'
+                                        '##', err
 
 # Checks pyramid with height: 8
 def test_generate_eight():
+    err = "Deine Funktion gibt für den Wert 8 eine falsche Pyramide aus"
     assert generate_mario_pyramid(8) == '       #\n' \
                                         '      ##\n' \
                                         '     ###\n' \
@@ -33,4 +42,4 @@ def test_generate_eight():
                                         '   #####\n' \
                                         '  ######\n' \
                                         ' #######\n' \
-                                        '########'
+                                        '########', err
